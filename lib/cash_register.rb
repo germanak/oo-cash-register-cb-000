a901683e0020665056dc17cd2b120dd3279e1f8e
Alec German <alec_german@yahoo.com>
2018-02-14 21:33:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+
+  def initialize(discount=20)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity=1)
+    quantity != 1 ? self.total += (price * quantity) : self.total += price
+  end
+
+  def apply_discount
+    self.total -= (self.total * (self.discount / 100))
+  end
+end
