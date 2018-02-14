337baeb02780affb915d51c560ce463eea956f2e
Alec German <alec_german@yahoo.com>
2018-02-14 21:54:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(item, price, quantity=1)
-    @item << item
+    @items << item
     quantity != 1 ? self.total += (price * quantity) : self.total += price
   end
 
